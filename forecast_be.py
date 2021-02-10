eeafa212afc24bab97cd982470edf8279a8a5872
Ananth Pallaseni <ap32@sanger.ac.uk>
2021-02-10 17:20:52 +0000

    added batch mode for fasta

--- forecast_be.py
+++ forecast_be.py
@@ -1,6 +1,7 @@
 import glob, os
 from joblib import load
 from sklearn.ensemble import GradientBoostingRegressor
+from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqUtils import MeltingTemp as mt
 import numpy as np 
@@ -101,7 +102,20 @@ def featurize_20nt_target(target_seq):
 
     return feature_labels, features
 
+def scale_zscores(zscores, mean, std):
+    scaled = []
+    for z in zscores:
+        if (z is None) or np.isnan(z):
+            s = z 
+        else:
+            s = (z * std) + mean
+            s = np.clip(s, 0, 1)
+        scaled.append(s)
+    return scaled
+
 def predict(target_seq, mean=None, std=None):
+    if len(target_seq) != 20:
+        raise ValueError(f'Input sequence is {len(target_seq)}, but require 20')
     feature_labels, features = featurize_20nt_target(target_seq)
     features = np.array(features).reshape(1, -1)
 
@@ -110,13 +124,44 @@ def predict(target_seq, mean=None, std=None):
     
     # Rescale if required
     if (mean is not None) and (std is not None):
-        predictions = [(pred *std)+mean for pred in predictions]
+        predictions = scale_zscores(predictions, mean, std)
         
     ret = [(pos, pred) for pos, pred in zip(POS_LIST, predictions)]
     
     # Overwrite predictions where there is no C at position
     ret = [(pos, pred) if target_seq[pos-1] == 'C' else (pos, None) for pos, pred in zip(POS_LIST, predictions)]
     return ret
+
+def predict_batch_fasta(fasta_path, output_path=None, mean=None, std=None):
+    zscore_predictions = []
+    guides = []
+    for record in SeqIO.parse(fasta_path, 'fasta'):
+        if (len(record.seq) != 20) or ('N' in record.seq):
+            continue
+        preds = [pred for pos, pred in predict(record.seq)]
+        zscore_predictions.append(preds)
+        guides.append(record.id)
+
+    df = pd.DataFrame(zscore_predictions, index=guides, columns=[f'Pos {pos} Zscore' for pos in POS_LIST])
+
+    # Scale if required
+    if (mean is not None) and (std is not None):
+        scaled_predictions = [scale_zscores(p, mean, std) for p in zscore_predictions]
+        scaled_df = pd.DataFrame(scaled_predictions, index=guides, columns=[f'Pos {pos} Scaled' for pos in POS_LIST])
+        df = pd.concat([df, scaled_df], axis=1)
+        df['Mean'] = mean
+        df['Std Dev'] = std
+
+    df.index.name = 'Oligo Id'
+
+    if output_path is not None:
+        df.to_csv(output_path)
+
+    return df
+
+
+
+    
 
 
 load_models()
