ff7c9016638a5e4a568002edc70b0cfc279c6038
Ananth Pallaseni <ap32@sanger.ac.uk>
2021-05-06 16:22:41 +0100

    added FNLS support

--- forecast_be.py
+++ forecast_be.py
@@ -13,8 +13,7 @@ try:
     assert os.path.exists(MODEL_DIR)
 except:
     raise FileNotFoundError(f'Could not find saved models. Looked in: {MODEL_DIR}')
-POS_MODELS = []
-POS_LIST = []
+POS_MODELS = {}
 POS_FRACS = {
     2: 0.05184948248657137,
     3: 0.19673314654134325,
@@ -29,12 +28,19 @@ POS_FRACS = {
 
 def load_models():
     global POS_MODELS
-    model_paths = sorted(glob.glob(MODEL_DIR + '/*'), key=lambda x: int(x.split('.')[0].split('_')[-1]))
-    for path in model_paths:
-        pos = int(path.split('.')[0].split('_')[-1])
-        model = load(path)
-        POS_LIST.append(pos)
-        POS_MODELS.append(model)
+    editors = [os.path.basename(d) for d in glob.glob(MODEL_DIR + '/*')]
+    for editor in editors:
+        model_paths = sorted(glob.glob(MODEL_DIR + f'{editor}/*'), key=lambda x: int(x.split('.')[0].split('_')[-1]))
+        pos_list = []
+        pos_models = []
+        for path in model_paths:
+            pos = int(path.split('.')[0].split('_')[-1])
+            model = load(path)
+            pos_list.append(pos)
+            pos_models.append(model)
+        POS_MODELS[editor] = {}
+        POS_MODELS[editor]['pos_list'] = pos_list
+        POS_MODELS[editor]['pos_models'] = pos_models
 
 # Microhomology features
 def generate_microhomology_matrix(seq):
@@ -107,26 +113,44 @@ def scale_zscores(zscores, pos_list, mean, std):
         scaled.append(s)
     return scaled
 
-def predict(target_seq, mean=None, std=None):
+def predict(target_seq, mean=None, std=None, editor=None):
     if len(target_seq) != 20:
         raise ValueError(f'Input sequence is {len(target_seq)}, but require 20')
     feature_labels, features = featurize_20nt_target(target_seq)
     features = np.array(features).reshape(1, -1)
 
+    # Get models for editor
+    if editor is None:
+        editor = 'BE4'
+    elif editor not in POS_MODELS:
+        raise ValueError(f'Unknown editor: {editor}')
+    pos_models = POS_MODELS[editor]['pos_models']
+    pos_list = POS_MODELS[editor]['pos_list']
+
     # Predict per position
-    predictions = [model.predict(features)[0] for model in POS_MODELS]
+    predictions = [model.predict(features)[0] for model in pos_models]
     
     # Rescale if required
     if (mean is not None) and (std is not None):
-        predictions = scale_zscores(predictions, POS_LIST, mean, std)
+        predictions = scale_zscores(predictions, pos_list, mean, std)
         
-    ret = [(pos, pred) for pos, pred in zip(POS_LIST, predictions)]
+    ret = [(pos, pred) for pos, pred in zip(pos_list, predictions)]
     
     # Overwrite predictions where there is no C at position
-    ret = [(pos, pred) if target_seq[pos-1] == 'C' else (pos, None) for pos, pred in zip(POS_LIST, predictions)]
+    ret = [(pos, pred) if target_seq[pos-1] == 'C' else (pos, None) for pos, pred in zip(pos_list, predictions)]
     return ret
 
-def predict_batch_fasta(fasta_path, output_path=None, mean=None, std=None):
+def predict_batch_fasta(fasta_path, output_path=None, mean=None, std=None, editor=None):
+    # Get models for editor
+    if editor is None:
+        editor = 'BE4'
+    elif editor not in POS_MODELS:
+        raise ValueError(f'Unknown editor: {editor}')
+    else:
+        pos_models = POS_MODELS[editor]['pos_models']
+        pos_list = POS_MODELS[editor]['pos_list']
+
+    # Predict per position for each guide
     zscore_predictions = []
     guides = []
     for record in SeqIO.parse(fasta_path, 'fasta'):
@@ -136,12 +160,12 @@ def predict_batch_fasta(fasta_path, output_path=None, mean=None, std=None):
         zscore_predictions.append(preds)
         guides.append(record.id)
 
-    df = pd.DataFrame(zscore_predictions, index=guides, columns=[f'Pos {pos} Zscore' for pos in POS_LIST])
+    df = pd.DataFrame(zscore_predictions, index=guides, columns=[f'Pos {pos} Zscore' for pos in pos_list])
 
     # Scale if required
     if (mean is not None) and (std is not None):
-        scaled_predictions = [scale_zscores(p, POS_LIST, mean, std) for p in zscore_predictions]
-        scaled_df = pd.DataFrame(scaled_predictions, index=guides, columns=[f'Pos {pos} Scaled' for pos in POS_LIST])
+        scaled_predictions = [scale_zscores(p, pos_list, mean, std) for p in zscore_predictions]
+        scaled_df = pd.DataFrame(scaled_predictions, index=guides, columns=[f'Pos {pos} Scaled' for pos in pos_list])
         df = pd.concat([df, scaled_df], axis=1)
         df['Mean'] = mean
         df['Std Dev'] = std
